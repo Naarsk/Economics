94ef6ff71fac67acd4614a7feae98748cf29b584
Leonardo Cruciani <50495467+Naarsk@users.noreply.github.com>
2025-02-03 13:34:00 +0100

    Add stuff about the 'rona

--- Econometrics/Coronavirus/code/read_data.py
+++ Econometrics/Coronavirus/code/read_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df= pd.read_csv('../WHO-COVID-19-global-daily-data.csv')
+df= pd.read_csv('../data/WHO-COVID-19-global-daily-data.csv')
 start_date = '2020-02-15'
 end_date = '2020-07-15'
 italy_df = df[(df['Country'] == 'Italy') &
@@ -10,3 +10,6 @@ dta=pd.DataFrame(italy_df['New_cases'].fillna(0))
 dta.insert(1, 'New_cases_lag_1', dta['New_cases'].shift(1).fillna(0))
 dta.insert(2, 'New_cases_lag_2', dta['New_cases'].shift(2).fillna(0))
 dta.insert(3, 'New_cases_lag_3', dta['New_cases'].shift(3).fillna(0))
+
+# Export the DataFrame to a Stata .dta file.
+dta.to_stata('../data/italy_data.dta', write_index=False)
